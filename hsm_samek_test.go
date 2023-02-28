982b23f0ef683ab777da77cf94a98ff8c89e1e35
Dragoslav Mitrinovic <12192438+dragomit@users.noreply.github.com>
2023-02-27 22:38:32 -0600

    more tests

---
+++ hsm_samek_test.go
@@ -0,0 +1,220 @@
+package hsm
+
+// This file implements the example state machine described in Miro Samek's book
+// "Practical Statecharts in C/C++" on page 95.
+// See https://www.state-machine.com/doc/PSiCC.pdf
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const (
+	evA = iota
+	evB
+	evC
+	evD
+	evE
+	evF
+	evG
+	evH
+	evInit
+)
+
+type hs struct {
+	foo bool
+}
+
+func (h *hs) setFoo(e Event) {
+	h.foo = true
+}
+
+func (h *hs) unsetFoo(e Event) {
+	h.foo = false
+}
+
+func (h *hs) isFoo(e Event) bool {
+	return h.foo
+}
+
+func (h *hs) isNotFoo(e Event) bool {
+	return !h.foo
+}
+
+func TestHsm(t *testing.T) {
+
+	var buf bytes.Buffer
+
+	makeA := func(txt string) func(Event, *hs) {
+		return func(Event, *hs) {
+			buf.WriteString(txt)
+			buf.WriteByte('\n')
+		}
+	}
+
+	h := hs{}
+	sm := StateMachine[*hs]{LocalDefault: true}
+
+	s0 := sm.State("s0").Entry(makeA("enter s0")).Exit(makeA("exit S0")).Initial().Build()
+
+	s1 := s0.State("s1").Initial().Entry(makeA("enter s1")).Exit(makeA("exit s1")).Build()
+
+	s11 := s1.State("s11").Initial().Entry(makeA("enter s11")).Exit(makeA("exit s11")).Build()
+	s2 := s0.State("s2").Entry(makeA("enter s2")).Exit(makeA("exit s2")).Build()
+	s21 := s2.State("s21").Initial().Entry(makeA("enter s21")).Exit(makeA("exit s21")).Build()
+	s211 := s21.State("s211").Initial().Entry(makeA("enter s211")).Exit(makeA("exit s211")).Build()
+
+	s0.AddTransition(evE, s211)
+
+	s1.AddTransition(evD, s0)
+	s1.AddTransition(evA, s1)
+	s1.AddTransition(evC, s2)
+
+	s11.Transition(evH, s11).Internal().Guard("is foo", func(event Event, h *hs) bool { return h.isFoo(event) }).Build()
+	s11.AddTransition(evG, s211)
+
+	s2.AddTransition(evC, s1)
+	s2.AddTransition(evF, s11)
+
+	s21.Transition(evH, s21).
+		Guard("not foo", func(event Event, h *hs) bool { return h.isNotFoo(event) }).
+		Action("set foo", func(event Event, h *hs) { h.setFoo(event) }).
+		Build()
+
+	sm.Finalize()
+	fmt.Println(sm.DiagramPUML(func(i int) string {
+		return string([]byte{'A' + byte(i)})
+	}))
+
+	smi := StateMachineInstance[*hs]{SM: &sm, Ext: &h}
+	smi.Initialize(Event{evInit, nil})
+
+	buf.WriteString("event A\n")
+	smi.Deliver(Event{evA, nil})
+
+	buf.WriteString("event Ext\n")
+	smi.Deliver(Event{evE, nil})
+
+	buf.WriteString("event Ext\n")
+	smi.Deliver(Event{evE, nil})
+
+	buf.WriteString("event A\n")
+	smi.Deliver(Event{evA, nil})
+
+	buf.WriteString("event H\n")
+	smi.Deliver(Event{evH, nil})
+
+	buf.WriteString("event H\n")
+	smi.Deliver(Event{evH, nil})
+
+	want := `enter s0
+enter s1
+enter s11
+event A
+exit s11
+exit s1
+enter s1
+enter s11
+event Ext
+exit s11
+exit s1
+enter s2
+enter s21
+enter s211
+event Ext
+exit s211
+exit s21
+exit s2
+enter s2
+enter s21
+enter s211
+event A
+event H
+exit s211
+exit s21
+enter s21
+enter s211
+event H
+`
+	assert.Equal(t, want, buf.String())
+
+}
+
+type falseBuf struct{}
+
+func (f falseBuf) WriteString(s string) {}
+func (f falseBuf) WriteByte(b byte)     {}
+func (f falseBuf) Reset()               {}
+
+func BenchmarkHsm(b *testing.B) {
+
+	//var buf bytes.Buffer
+	var buf falseBuf
+
+	makeA := func(txt string) func(Event, *hs) {
+		return func(Event, *hs) {
+			buf.WriteString(txt)
+			buf.WriteByte('\n')
+		}
+	}
+
+	sm := StateMachine[*hs]{LocalDefault: true}
+
+	s0 := sm.State("s0").Entry(makeA("enter s0")).Exit(makeA("exit S0")).Initial().Build()
+
+	s1 := s0.State("s1").Initial().Entry(makeA("enter s1")).Exit(makeA("exit s1")).Build()
+
+	s11 := s1.State("s11").Initial().Entry(makeA("enter s11")).Exit(makeA("exit s11")).Build()
+	s2 := s0.State("s2").Entry(makeA("enter s2")).Exit(makeA("exit s2")).Build()
+	s21 := s2.State("s21").Initial().Entry(makeA("enter s21")).Exit(makeA("exit s21")).Build()
+	s211 := s21.State("s211").Initial().Entry(makeA("enter s211")).Exit(makeA("exit s211")).Build()
+
+	s0.AddTransition(evE, s211)
+
+	s1.AddTransition(evD, s0)
+	s1.AddTransition(evA, s1)
+	s1.AddTransition(evC, s2)
+
+	s11.Transition(evH, s11).Internal().Guard("is foo", func(event Event, h *hs) bool { return h.isFoo(event) }).Build()
+	s11.AddTransition(evG, s211)
+
+	s2.AddTransition(evC, s1)
+	s2.AddTransition(evF, s11)
+
+	s21.Transition(evH, s21).
+		Guard("not foo", func(event Event, h *hs) bool { return h.isNotFoo(event) }).
+		Action("set foo", func(event Event, h *hs) { h.setFoo(event) }).
+		Build()
+
+	sm.Finalize()
+
+	for i := 0; i < b.N; i++ {
+
+		buf.Reset()
+		h := hs{}
+
+		smi := StateMachineInstance[*hs]{SM: &sm, Ext: &h}
+		smi.Initialize(Event{evInit, nil})
+
+		buf.WriteString("event A\n")
+		smi.Deliver(Event{evA, nil})
+
+		buf.WriteString("event Ext\n")
+		smi.Deliver(Event{evE, nil})
+
+		buf.WriteString("event Ext\n")
+		smi.Deliver(Event{evE, nil})
+
+		buf.WriteString("event A\n")
+		smi.Deliver(Event{evA, nil})
+
+		buf.WriteString("event H\n")
+		smi.Deliver(Event{evH, nil})
+
+		buf.WriteString("event H\n")
+		smi.Deliver(Event{evH, nil})
+
+	}
+}
--- hsm_test.go
+++ hsm_test.go
@@ -1,216 +1,133 @@
-package hsm
+package hsm_test
 
 import (
 	"bytes"
-	"fmt"
+	"github.com/dragomit/hsm"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-const (
-	evA = iota
-	evB
-	evC
-	evD
-	evE
-	evF
-	evG
-	evH
-	evInit
-)
+func TestLocalInternalExternal(t *testing.T) {
 
-type hs struct {
-	foo bool
-}
-
-func (h *hs) setFoo(e Event) {
-	h.foo = true
-}
-
-func (h *hs) unsetFoo(e Event) {
-	h.foo = false
-}
-
-func (h *hs) isFoo(e Event) bool {
-	return h.foo
-}
-
-func (h *hs) isNotFoo(e Event) bool {
-	return !h.foo
-}
-
-func TestHsm(t *testing.T) {
+	const (
+		evA = iota
+		evB
+		evC
+		evD
+		evE
+		evF
+		evG
+		evH
+		evI
+	)
 
 	var buf bytes.Buffer
 
-	makeA := func(txt string) func(Event, *hs) {
-		return func(Event, *hs) {
+	// action-maker
+	makeA := func(txt string) func(hsm.Event, struct{}) {
+		return func(hsm.Event, struct{}) {
 			buf.WriteString(txt)
-			buf.WriteByte('\n')
+			buf.WriteByte('|')
 		}
 	}
 
-	h := hs{}
-	sm := StateMachine[*hs]{LocalDefault: true}
+	sm := hsm.StateMachine[struct{}]{}
+	s := sm.State("S").Entry(makeA("enter S")).Exit(makeA("exit S")).Initial().Build()
+	s1 := s.State("S1").Entry(makeA("enter S1")).Exit(makeA("exit S1")).Initial().Build()
+	s11 := s1.State("S11").Entry(makeA("enter S11")).Exit(makeA("exit S11")).Initial().Build()
+	s12 := s1.State("S12").Entry(makeA("enter S12")).Exit(makeA("exit S12")).Build()
 
-	s0 := sm.State("s0").Entry(makeA("enter s0")).Exit(makeA("exit S0")).Initial().Build()
+	s11.Transition(evA, s).Local(true).Action("A", makeA("action A")).Build()
+	s11.Transition(evB, s).Local(false).Action("B", makeA("action B")).Build()
 
-	s1 := s0.State("s1").Initial().Entry(makeA("enter s1")).Exit(makeA("exit s1")).Build()
+	s1.Transition(evC, s).Local(true).Action("C", makeA("action C")).Build()
+	s1.Transition(evD, s).Local(false).Action("D", makeA("action D")).Build()
 
-	s11 := s1.State("s11").Initial().Entry(makeA("enter s11")).Exit(makeA("exit s11")).Build()
-	s2 := s0.State("s2").Entry(makeA("enter s2")).Exit(makeA("exit s2")).Build()
-	s21 := s2.State("s21").Initial().Entry(makeA("enter s21")).Exit(makeA("exit s21")).Build()
-	s211 := s21.State("s211").Initial().Entry(makeA("enter s211")).Exit(makeA("exit s211")).Build()
+	s1.Transition(evE, s12).Local(true).Action("E", makeA("action E")).Build()
+	s1.Transition(evF, s12).Local(false).Action("F", makeA("action F")).Build()
 
-	s0.AddTransition(evE, s211)
-
-	s1.AddTransition(evD, s0)
-	s1.AddTransition(evA, s1)
-	s1.AddTransition(evC, s2)
-
-	s11.Transition(evH, s11).Internal().Guard("is foo", func(event Event, h *hs) bool { return h.isFoo(event) }).Build()
-	s11.AddTransition(evG, s211)
-
-	s2.AddTransition(evC, s1)
-	s2.AddTransition(evF, s11)
-
-	s21.Transition(evH, s21).
-		Guard("not foo", func(event Event, h *hs) bool { return h.isNotFoo(event) }).
-		Action("set foo", func(event Event, h *hs) { h.setFoo(event) }).
-		Build()
+	s.Transition(evG, nil).Action("G", makeA("action G")).Build()
+	s1.Transition(evH, s1).Internal().Action("H-in-S1", makeA("H-in-S1")).Build()
+	s12.Transition(evH, s12).Internal().Action("H-in-S12", makeA("H-in-S12")).Build()
+	s11.Transition(evI, s11).Action("I", makeA("action I")).Build()
 
 	sm.Finalize()
-	fmt.Println(sm.DiagramPUML(func(i int) string {
-		return string([]byte{'A' + byte(i)})
-	}))
 
-	smi := StateMachineInstance[*hs]{SM: &sm, Ext: &h}
-	smi.Initialize(Event{evInit, nil})
-
-	buf.WriteString("event A\n")
-	smi.Deliver(Event{evA, nil})
-
-	buf.WriteString("event Ext\n")
-	smi.Deliver(Event{evE, nil})
-
-	buf.WriteString("event Ext\n")
-	smi.Deliver(Event{evE, nil})
-
-	buf.WriteString("event A\n")
-	smi.Deliver(Event{evA, nil})
-
-	buf.WriteString("event H\n")
-	smi.Deliver(Event{evH, nil})
-
-	buf.WriteString("event H\n")
-	smi.Deliver(Event{evH, nil})
-
-	want := `enter s0
-enter s1
-enter s11
-event A
-exit s11
-exit s1
-enter s1
-enter s11
-event Ext
-exit s11
-exit s1
-enter s2
-enter s21
-enter s211
-event Ext
-exit s211
-exit s21
-exit s2
-enter s2
-enter s21
-enter s211
-event A
-event H
-exit s211
-exit s21
-enter s21
-enter s211
-event H
-`
-	assert.Equal(t, want, buf.String())
-
-}
-
-type falseBuf struct{}
-
-func (f falseBuf) WriteString(s string) {}
-func (f falseBuf) WriteByte(b byte)     {}
-func (f falseBuf) Reset()               {}
-
-func BenchmarkHsm(b *testing.B) {
-
-	//var buf bytes.Buffer
-	var buf falseBuf
-
-	makeA := func(txt string) func(Event, *hs) {
-		return func(Event, *hs) {
-			buf.WriteString(txt)
-			buf.WriteByte('\n')
-		}
+	tests := []struct {
+		name    string
+		events  []int
+		actions string
+		state   *hsm.State[struct{}]
+	}{
+		{
+			name:    "local transitions",
+			events:  []int{evA, evE},
+			actions: "enter S|enter S1|enter S11|exit S11|exit S1|action A|enter S1|enter S11|exit S11|action E|enter S12|",
+			state:   s12,
+		},
+		{
+			name:    "external transitions",
+			events:  []int{evB, evF},
+			actions: "enter S|enter S1|enter S11|exit S11|exit S1|exit S|action B|enter S|enter S1|enter S11|exit S11|exit S1|action F|enter S1|enter S12|",
+			state:   s12,
+		},
+		{
+			name:    "local transitions 2",
+			events:  []int{evE, evC},
+			actions: "enter S|enter S1|enter S11|exit S11|action E|enter S12|exit S12|exit S1|action C|enter S1|enter S11|",
+			state:   s11,
+		},
+		{
+			name:    "external transitions 2",
+			events:  []int{evD},
+			actions: "enter S|enter S1|enter S11|exit S11|exit S1|exit S|action D|enter S|enter S1|enter S11|",
+			state:   s11,
+		},
+		{
+			name:    "termination",
+			events:  []int{evG, evA, evB, evC, evD, evE, evF},
+			actions: "enter S|enter S1|enter S11|exit S11|exit S1|exit S|action G|",
+			state:   nil,
+		},
+		{
+			name:    "internal 1",
+			events:  []int{evH},
+			actions: "enter S|enter S1|enter S11|H-in-S1|",
+			state:   s11,
+		},
+		{
+			name:    "internal 2",
+			events:  []int{evE, evH},
+			actions: "enter S|enter S1|enter S11|exit S11|action E|enter S12|H-in-S12|",
+			state:   s12,
+		},
+		{
+			name:    "external self-transition",
+			events:  []int{evI},
+			actions: "enter S|enter S1|enter S11|exit S11|action I|enter S11|",
+			state:   s11,
+		},
 	}
 
-	sm := StateMachine[*hs]{LocalDefault: true}
-
-	s0 := sm.State("s0").Entry(makeA("enter s0")).Exit(makeA("exit S0")).Initial().Build()
-
-	s1 := s0.State("s1").Initial().Entry(makeA("enter s1")).Exit(makeA("exit s1")).Build()
-
-	s11 := s1.State("s11").Initial().Entry(makeA("enter s11")).Exit(makeA("exit s11")).Build()
-	s2 := s0.State("s2").Entry(makeA("enter s2")).Exit(makeA("exit s2")).Build()
-	s21 := s2.State("s21").Initial().Entry(makeA("enter s21")).Exit(makeA("exit s21")).Build()
-	s211 := s21.State("s211").Initial().Entry(makeA("enter s211")).Exit(makeA("exit s211")).Build()
-
-	s0.AddTransition(evE, s211)
-
-	s1.AddTransition(evD, s0)
-	s1.AddTransition(evA, s1)
-	s1.AddTransition(evC, s2)
-
-	s11.Transition(evH, s11).Internal().Guard("is foo", func(event Event, h *hs) bool { return h.isFoo(event) }).Build()
-	s11.AddTransition(evG, s211)
-
-	s2.AddTransition(evC, s1)
-	s2.AddTransition(evF, s11)
-
-	s21.Transition(evH, s21).
-		Guard("not foo", func(event Event, h *hs) bool { return h.isNotFoo(event) }).
-		Action("set foo", func(event Event, h *hs) { h.setFoo(event) }).
-		Build()
-
-	sm.Finalize()
-
-	for i := 0; i < b.N; i++ {
-
-		buf.Reset()
-		h := hs{}
-
-		smi := StateMachineInstance[*hs]{SM: &sm, Ext: &h}
-		smi.Initialize(Event{evInit, nil})
-
-		buf.WriteString("event A\n")
-		smi.Deliver(Event{evA, nil})
-
-		buf.WriteString("event Ext\n")
-		smi.Deliver(Event{evE, nil})
-
-		buf.WriteString("event Ext\n")
-		smi.Deliver(Event{evE, nil})
-
-		buf.WriteString("event A\n")
-		smi.Deliver(Event{evA, nil})
-
-		buf.WriteString("event H\n")
-		smi.Deliver(Event{evH, nil})
-
-		buf.WriteString("event H\n")
-		smi.Deliver(Event{evH, nil})
-
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			smi := hsm.StateMachineInstance[struct{}]{SM: &sm}
+			buf.Reset()
+			smi.Initialize(hsm.Event{EventId: -1, Data: nil})
+			for _, ev := range test.events {
+				smi.Deliver(hsm.Event{EventId: ev, Data: nil})
+			}
+			if smi.Current() != test.state {
+				wants, got := "nil", "nil"
+				if test.state != nil {
+					wants = test.state.Name()
+				}
+				if smi.Current() != nil {
+					got = smi.Current().Name()
+				}
+				t.Errorf("Wants state %s got %s", wants, got)
+			}
+			assert.Equal(t, buf.String(), test.actions)
+		})
 	}
 }
